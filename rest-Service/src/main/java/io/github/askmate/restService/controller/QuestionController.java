ddf29c0b93174f7518e6847217d6927dc747088d
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-02-19 13:09:18 +0100

    added questioncontroller content

--- rest-Service/src/main/java/io/github/askmate/restService/controller/QuestionController.java
+++ rest-Service/src/main/java/io/github/askmate/restService/controller/QuestionController.java
@@ -1,4 +1,23 @@
 package io.github.askmate.restService.controller;
 
+
+import io.github.askmate.restService.model.QuestionDTO;
+import io.github.askmate.restService.service.QuestionService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
 public class QuestionController {
+
+    private QuestionService questionService;
+
+    @GetMapping
+    public List<QuestionDTO> getAllQuestions(){
+        return null;
+    }
 }
+
