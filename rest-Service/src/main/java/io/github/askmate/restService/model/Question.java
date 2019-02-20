97e87721f26b8beb68ee90a69a38c9462a1284a6
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-02-20 18:17:41 +0100

    uncomment question class

--- rest-Service/src/main/java/io/github/askmate/restService/model/Question.java
+++ rest-Service/src/main/java/io/github/askmate/restService/model/Question.java
@@ -1,14 +1,14 @@
 package io.github.askmate.restService.model;
 
 
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//
-//@Data
-//@AllArgsConstructor
-//public class Question {
-//
-//    String description;
-//    int id;
-//
-//}
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Question {
+
+    String description;
+    int id;
+
+}
