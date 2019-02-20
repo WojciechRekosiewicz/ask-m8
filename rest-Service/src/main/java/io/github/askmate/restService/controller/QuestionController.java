caefa5f47610d426901d89e0eeee9d692e6ed45c
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-02-20 18:09:04 +0100

    getAllQuestion added to controller

--- rest-Service/src/main/java/io/github/askmate/restService/controller/QuestionController.java
+++ rest-Service/src/main/java/io/github/askmate/restService/controller/QuestionController.java
@@ -2,8 +2,10 @@ package io.github.askmate.restService.controller;
 
 
 import io.github.askmate.restService.model.QuestionDTO;
+import io.github.askmate.restService.model.QuestionDao;
 import io.github.askmate.restService.service.QuestionService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +17,14 @@ import java.util.List;
 @RequestMapping(value = "/questions")
 public class QuestionController {
 
+    @Autowired
+    private QuestionDao questionDao;
 
-    private QuestionService questionService;
+   // private QuestionService questionService;
 
     @GetMapping
     public List<QuestionDTO> getAllQuestions(){
-     return questionService.getAllQuestion();
+     return questionDao.getAllQuestion();
     }
 }
 
